bc401ffb1dd2f1d7d55472de2607c8558aad2750
Rohit Joshi <rohit.joshi@tieto.com>
2019-07-31 12:46:35 +0530

    Added code base placeholders for ChoreApp

---
+++ ChoreApp/app/src/main/java/com/rohit/kotlin/choreapp/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.rohit.kotlin.choreapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
