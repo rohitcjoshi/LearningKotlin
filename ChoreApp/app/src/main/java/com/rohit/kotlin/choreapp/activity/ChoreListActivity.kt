23a393b91f3334ddf0fe6da2838ed8a4df57dd90
Rohit Joshi <rohit.joshi@tieto.com>
2019-08-20 15:47:11 +0530

    Added popup dialog for entering new chore

--- ChoreApp/app/src/main/java/com/rohit/kotlin/choreapp/activity/ChoreListActivity.kt
+++ ChoreApp/app/src/main/java/com/rohit/kotlin/choreapp/activity/ChoreListActivity.kt
@@ -1,7 +1,13 @@
 package com.rohit.kotlin.choreapp.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.rohit.kotlin.choreapp.R
@@ -9,6 +15,7 @@ import com.rohit.kotlin.choreapp.data.ChoreListAdapter
 import com.rohit.kotlin.choreapp.data.ChoresDBHandler
 import com.rohit.kotlin.choreapp.model.Chore
 import kotlinx.android.synthetic.main.activity_chore_list.*
+import kotlinx.android.synthetic.main.popup_dialog.view.*
 
 class ChoreListActivity : AppCompatActivity() {
 
@@ -17,6 +24,9 @@ class ChoreListActivity : AppCompatActivity() {
     private var choreList: ArrayList<Chore>? = null
     private var savedChoreList: ArrayList<Chore>? = null
     private var layoutManager: RecyclerView.LayoutManager? = null
+
+    private var dialogBuilder: AlertDialog.Builder ? = null
+    private var alertDialog: AlertDialog? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,17 +44,47 @@ class ChoreListActivity : AppCompatActivity() {
         choresListView.layoutManager = layoutManager
         choresListView.adapter = adapter
 
-        // Load chores from DB
-        choreList = dbHandler!!.readAllChores()
-        for(ch in choreList!!.iterator()) {
-            val chore: Chore = Chore()
-            chore.choreName = ch.choreName
-            chore.assignedTo = ch.assignedTo
-            chore.assignedBy = ch.assignedBy
-            chore.timeAssigned = ch.timeAssigned
+        if(dbHandler!!.getChoresCount() > 0) {
+            // Load chores from DB
+            choreList = dbHandler!!.readAllChores()
+            for (ch in choreList!!.iterator()) {
+                val chore: Chore = Chore()
+                chore.choreName = ch.choreName
+                chore.assignedTo = ch.assignedTo
+                chore.assignedBy = ch.assignedBy
+                chore.timeAssigned = ch.timeAssigned
 
-            savedChoreList!!.add(chore)
+                savedChoreList!!.add(chore)
+            }
+            adapter!!.notifyDataSetChanged()
+        } else {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
         }
-        adapter!!.notifyDataSetChanged()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.top_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId == R.id.add_menu_button) {
+            Log.d("Menu Clicked", "Add menu clicked..!")
+            createPopupDialog()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    fun createPopupDialog() {
+        val view: View = layoutInflater.inflate(R.layout.popup_dialog, null)
+        val choreName = view.etPopupEnterChore
+        val assignedBy = view.etPopupAssignedBy
+        val assignedTo = view.etPopupAssignedTo
+        val btnSave = view.btnPopupSave
+
+        dialogBuilder = AlertDialog.Builder(this).setView(view)
+        alertDialog = dialogBuilder!!.create()
+        alertDialog?.show()
     }
 }
